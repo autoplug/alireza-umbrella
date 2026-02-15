e87e2f97b5a4ddaf75a004ad386dbece922b2832
Alireza <alireza.b83@gmail.com>
2026-02-15 11:19:30 +0330

    Update WalletList.jsx

--- src/components/WalletList.jsx
+++ src/components/WalletList.jsx
@@ -1,1 +1,18 @@
+import React from "react";
+import WalletItem from "./WalletItem";
 
+// Wallet list component
+function WalletList({ wallets }) {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>My Wallets</h2>
+
+      {/* Render each wallet item */}
+      {wallets.map((wallet) => (
+        <WalletItem key={wallet.id} wallet={wallet} />
+      ))}
+    </div>
+  );
+}
+
+export default WalletList;
