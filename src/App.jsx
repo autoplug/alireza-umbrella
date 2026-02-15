6dca9c39203223419493350378625c92003a4668
Alireza <alireza.b83@gmail.com>
2026-02-15 23:12:59 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
 import Home from "./pages/Home";
-import TokenInput from "./components/TokenInput";
+import Markets from "./pages/Markets";
+import Settings from "./pages/Settings";
+import BottomNav from "./layout/BottomNav";
 
 function App() {
-  const [token, setToken] = useState(localStorage.getItem("API_TOKEN") || "");
-
-  useEffect(() => {
-    if (token) localStorage.setItem("API_TOKEN", token);
-  }, [token]);
-
   return (
     <Router>
-      <div style={{ paddingBottom: "70px" }}> {/* paddingBottom to avoid overlap with Footer */}
+      <div style={{ paddingBottom: "100px" }}> {/* space for BottomNav */}
         <Routes>
-          <Route
-            path="/"
-            element={token ? <Home token={token} /> : <Navigate to="/token" replace />}
-          />
-          <Route
-            path="/token"
-            element={<TokenInput onTokenSubmit={(t) => setToken(t)} />}
-          />
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/markets" element={<Markets />} />
+          <Route path="/settings" element={<Settings />} />
         </Routes>
       </div>
 
+      <BottomNav />
     </Router>
   );
 }
