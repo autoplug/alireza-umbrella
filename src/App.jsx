8445eb7cd2d9868b2a4784fd1ad525f7f924ac1a
Alireza <alireza.b83@gmail.com>
2026-02-15 15:23:37 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import TokenInput from "./components/TokenInput";
-import Footer from "./components/Footer";
+import Footer from "./layout/Footer";
 
 function App() {
   const [token, setToken] = useState(localStorage.getItem("API_TOKEN") || "");
