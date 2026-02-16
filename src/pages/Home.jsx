38a5a3cec8c1ad7c0205005c569fc14a1c30ef97
Alireza <alireza.b83@gmail.com>
2026-02-16 16:25:15 +0330

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,11 @@ import React from "react";
 import WalletList from "../components/WalletList";
 import OrdersList from "../components/OrdersList";
 
-export default function Home({ wallets, orders }) {
+export default function Home({ wallets }) {
   return (
     <div>
       <h2>Wallets</h2>
       <WalletList wallets={wallets} />
-
-      <h2>Orders</h2>
-      <OrdersList orders={orders} />
     </div>
   );
 }
