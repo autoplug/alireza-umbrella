dac2f3f6f1a92448ee2d3caea0250629d89194d2
Alireza <alireza.b83@gmail.com>
2026-02-16 16:55:30 +0330

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,17 @@ import WalletList from "../components/WalletList";
 export default function Home({ wallets }) {
   return (
     <div>
+      
+      
+      
+      <div>
+  <h3>Wallet Debug:</h3>
+  <pre>
+    {JSON.stringify(wallets, null, 2)}
+  </pre>
+</div>
+      
+      
       <h2>Wallets</h2>
       <WalletList wallets={wallets} />
     </div>
