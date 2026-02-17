7b8b8c997bf2aea406f128abc2589913f6709e87
Alireza <alireza.b83@gmail.com>
2026-02-17 19:06:11 +0330

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import WalletList from "../components/WalletList";
 import ActiveOrders from "../components/ActiveOrders";
 export default function Home() {
   return (
-    <div style={{ padding: 16, maxWidth: 800, margin: "0 auto" }}>
+    <div>
       <WalletList />
       <ActiveOrders />
     </div>
