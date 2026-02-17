46dc34432468407a8a405202a9d4880ded5094ec
Alireza <alireza.b83@gmail.com>
2026-02-18 00:20:14 +0330

    Update TitleBar.jsx

--- src/components/TitleBar.jsx
+++ src/components/TitleBar.jsx
@@ -5,7 +5,7 @@ export default function TitleBar({ title, count }) {
     backgroundColor: "#f5e1c9",       // کرم روشن
     borderTop: "2px solid #d9bfae",   // کرم تیره‌تر
     borderBottom: "2px solid #d9bfae",
-    padding: "12px 10px",
+    padding: "18px 10px",
     margin: "16px 0",
     marginBottom : "3px",
     display: "flex",
