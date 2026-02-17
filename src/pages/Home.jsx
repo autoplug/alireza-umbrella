f708d9191859dbd15e66336f4271616c49caa3e0
Alireza <alireza.b83@gmail.com>
2026-02-17 16:35:43 +0330

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import WalletList from "../components/WalletList";
-import DebugPanel from "../components/DebugPanel";
+
 export default function Home() {
   return (
     <div style={{ padding: 16, maxWidth: 800, margin: "0 auto" }}>
       <h1>Wallets</h1>
-      <DebugPanel />
       <WalletList />
     </div>
   );
