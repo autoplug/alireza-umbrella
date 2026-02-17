449d8e13b74c5c3fa318c4bbb1962df4c550cf43
Alireza <alireza.b83@gmail.com>
2026-02-17 22:16:53 +0330

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import WalletList from "../components/WalletList";
 import ActiveOrders from "../components/ActiveOrders";
+import TodayTrades from "../components/TodayTrades";
+
 export default function Home() {
   return (
     <div>
       <WalletList />
       <ActiveOrders />
+      <TodayTrades />
     </div>
   );
 }
