d0792e0abb4d6a515a3ba07e6df659134e2ffb63
Alireza <alireza.b83@gmail.com>
2026-02-17 18:06:06 +0330

    Update Header.jsx

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -53,7 +53,7 @@ export default function Header() {
   return (
     <div
       style={{
-        backgroundColor: "#222",
+        backgroundColor: "#808080",
         color: "#fff",
         padding: "8px 1px",
         fontFamily: "monospace",
