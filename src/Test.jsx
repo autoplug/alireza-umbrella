0228d7ab2598c7aa7fc3dda12bc118656600d2cf
Alireza <alireza.b83@gmail.com>
2026-02-17 10:38:59 +0330

    Create Test.jsx

---
+++ src/Test.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div style={{ padding: 16, maxWidth: 800, margin: "0 auto" }}>
+      <h1>Wallets</h1>
+     
+    </div>
+  );
+}
