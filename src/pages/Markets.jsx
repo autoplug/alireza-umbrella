320cd5639c4340636b9c02cefa5cfd4c18c791b8
Alireza <alireza.b83@gmail.com>
2026-02-18 14:04:25 +0330

    Update Markets.jsx

--- src/pages/Markets.jsx
+++ src/pages/Markets.jsx
@@ -1,17 +1,13 @@
 import React from "react";
-import OrdersList from "../components/OrdersList";
-import MarketsList from "../components/MarketsList"; // if you prefer a Markets component
+import ProfitSummary from "../components/ProfitSummary"; // if you prefer a Markets component
 
 export default function MarketPage() {
   return (
-    <div style={{ padding: 16, maxWidth: 1000, margin: "0 auto" }}>
-      {/* Orders section */}
-      <h1>Orders</h1>
-      <OrdersList />
+    <div>
+      {/* ProfitSummary section */}
+      <ProfitSummary />
+
       
-      {/* Markets section */}
-      <h1 style={{ marginTop: 32 }}>Markets</h1>
-      <MarketsList />
 
     </div>
   );
