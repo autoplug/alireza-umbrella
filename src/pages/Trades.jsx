b855c4377152a7b2be3834b618d5d0f8a6abd0b7
Alireza <alireza.b83@gmail.com>
2026-02-18 23:01:00 +0330

    Update Trades.jsx

--- src/pages/Trades.jsx
+++ src/pages/Trades.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ProfitSummary from "../components/ProfitSummary"; // if you prefer a Markets component
+import DebugPanel from "../components/ProfitSummary";
 
 export default function MarketPage() {
   return (
     <div>
+      <DebugPanel />
       {/* ProfitSummary section */}
       <ProfitSummary />
   
