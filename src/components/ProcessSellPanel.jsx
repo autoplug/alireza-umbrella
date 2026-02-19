8634b1519be713e6f53bcf108a1d6489b7a1ced6
Alireza <alireza.b83@gmail.com>
2026-02-19 23:04:13 +0330

    Update ProcessSellPanel.jsx

--- src/components/ProcessSellPanel.jsx
+++ src/components/ProcessSellPanel.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 import {
-  processSell,
+  processAllSells,
   removeDuplicates,
-  weightedAveragePrice,
 } from "../api/utils";
 
 import localOrders from "../assets/nobitex.json";
