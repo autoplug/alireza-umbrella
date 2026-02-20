140dd42b91873f648a26af40df958ee223ef2509
Alireza <alireza.b83@gmail.com>
2026-02-20 15:05:37 +0330

    Update WalletList.jsx

--- src/components/WalletList.jsx
+++ src/components/WalletList.jsx
@@ -93,7 +93,7 @@ export default function WalletList() {
             <MarketIcon market={wallet.currency.toUpperCase()} size="large" />
 
             {/* Right: Amounts */}
-            <div style={{ display: "flex", flexDirection: "column" marginLeft: "50px"}}>
+            <div style={{ display: "flex", flexDirection: "column", marginLeft: "50px"}}>
               <div style={{ fontWeight: 700, fontSize: 16  }}>
                 {formatBalance(wallet.balance, wallet.currency)}
               </div>
