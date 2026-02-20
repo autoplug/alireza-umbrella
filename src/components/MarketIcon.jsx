b84472e9e5999ead8d1c21bf8b6470e2d432e5da
Alireza <alireza.b83@gmail.com>
2026-02-20 17:53:50 +0330

    Update MarketIcon.jsx

--- src/components/MarketIcon.jsx
+++ src/components/MarketIcon.jsx
@@ -93,7 +93,7 @@ export default function MarketIcon({ market, size = "normal" }) {
           maxWidth: 120,
         }}
       >
-        {market.padEnd(4,"&nbsp;")}
+        {market.padEnd(4," ")}
       </div>
     </div>
   );
