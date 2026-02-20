53aae6292e25216bfb74bfe1f582b88f2d578b87
Alireza <alireza.b83@gmail.com>
2026-02-20 19:52:02 +0330

    Update Chart.jsx

--- src/pages/Chart.jsx
+++ src/pages/Chart.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import TokenInput from "../components/TokenInput";
+import CandleStick from "../components/CandleStick";
 
 export default function Chart() {
   const [token, setToken] = useState(localStorage.getItem("NOBITEX_TOKEN") || "");
@@ -12,7 +12,7 @@ export default function Chart() {
   return (
     <div style={{ padding: 16 }}>
       <h2>Chart</h2>
-      <TokenInput initialToken={token} onSave={handleTokenSave} />
+      <CandleStick />
       {/* Additional settings can be added here */}
     </div>
   );
