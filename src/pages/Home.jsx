6967ee32fa4c4ee4b110e9648c7f9de51fbdc475
Alireza <alireza.b83@gmail.com>
2026-02-23 23:33:19 +0330

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import WalletList from "../components/WalletList";
 //import ActiveOrders from "../components/ActiveOrders";
-//import TodayTrades from "../components/TodayTrades";
+import TodayTrades from "../components/TodayTrades";
 
 export default function Home() {
   return (
     <div>
       
       <WalletList />
-      {/*<TodayTrades />*/}
+      <TodayTrades />
       {/*<ActiveOrders />*/}
 
       {/* Spacer at the bottom to avoid overlapping with bottom menu */}
