fb4d414c022fa44daf78cc18b13b928c4f4cd2ea
Alireza <alireza.b83@gmail.com>
2026-02-23 22:48:09 +0330

    Create useHistory.jsx

---
+++ src/hooks/useHistory.jsx
@@ -0,0 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchHistory } from "../api/fetchHistory";
+
+export const useHistory = () => {
+  const query = useQuery({
+    queryKey: ["candles"],
+    queryFn: () => fetchHistory(),
+
+    staleTime: 5 * 60 * 1000,  // 5 minutes
+    gcTime: Infinity,           // keep cache forever
+    refetchOnMount: false,
+    refetchOnWindowFocus: true,
+    refetchOnReconnect: true,
+    placeholderData: (prev) => prev, // show previous data while fetching
+  });
+  
+  // Safe access to data
+  const candles = query.data?.candles || [];
+  const lastUpdate = query.data?._lastUpdate || null;
+
+  return { ...query, candles, lastUpdate };
+  
+};
