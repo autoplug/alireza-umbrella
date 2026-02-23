4cc0f2c5e38121f1625e50dc33e759c7e6062c47
Alireza <alireza.b83@gmail.com>
2026-02-23 18:55:58 +0330

    Update fetchTrades.jsx

--- src/api/fetchTrades.jsx
+++ src/api/fetchTrades.jsx
@@ -4,31 +4,50 @@ import axios from "axios";
 const WORKER_URL = "https://nobitex.alireza-b83.workers.dev";
 
 export const fetchTrades = async ({ onUpdate } = {}) => {
-  const token = localStorage.getItem("NOBITEX_TOKEN");
-  if (!token) return [];
-
-  const headers = { Authorization: `Token ${token}` };
-  let allTrades = [];
-  let page = 1;
-  let hasMore = true;
-
-  while (hasMore) {
-    const response = await axios.get(`${WORKER_URL}/market/trades/list`, {
-      params: { page, pageSize: 100 },
-      headers,
-      validateStatus: () => true,
-    });
-
-    const trades = response.data?.trades || [];
-
-    if (trades.length > 0) {
-      allTrades = [...allTrades, ...trades];
-      page++;
-      await new Promise((resolve) => setTimeout(resolve, 2000)); // 2s delay
-    } else {
-      hasMore = false;
+  try
+    
+    
+    const token = localStorage.getItem("NOBITEX_TOKEN");
+    if (!token) return [];
+  
+    const headers = { Authorization: `Token ${token}` };
+    let allTrades = [];
+    let page = 1;
+    let hasMore = true;
+  
+    while (hasMore) {
+      const response = await axios.get(`${WORKER_URL}/market/trades/list`, {
+        params: { page, pageSize: 100 },
+        headers,
+        validateStatus: () => true,
+      });
+  
+      const trades = response.data?.trades || [];
+  
+      if (trades.length > 0) {
+        allTrades = [...allTrades, ...trades];
+        page++;
+        await new Promise((resolve) => setTimeout(resolve, 2000)); // 2s delay
+      } else {
+        hasMore = false;
+      }
     }
+
+
+    const lastUpdate = Date.now();
+
+    // Callback for Header or other components
+    if (typeof onUpdate === "function") {
+      onUpdate({ allTrades, _lastUpdate: lastUpdate });
+    }
+
+    return { allTrades, _lastUpdate: lastUpdate };
+  } catch (err) {
+    console.error("fetchOrders failed:", err);
+    return { allTrades: [], _lastUpdate: null };
   }
 
-  return allTrades;
+
+
+
 };
