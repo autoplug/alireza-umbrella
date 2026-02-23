5095c3d6db765800393c7102e13f424b8af593be
Alireza <alireza.b83@gmail.com>
2026-02-23 14:25:27 +0330

    Create fetchMarkets.jsx

---
+++ src/api/fetchMarkets.jsx
@@ -0,0 +1,79 @@
+import axios from "axios";
+
+const WORKER_URL = "https://nobitex.alireza-b83.workers.dev";
+const CACHE_KEY = "WALLETS_CACHE";
+const CACHE_TIME_KEY = "WALLETS_CACHE_TIME";
+// 5 minutes
+const MIN_FETCH_INTERVAL = 5 * 60 * 1000;
+
+// ---------------- CACHE HELPERS ----------------
+const getCache = () => {
+  const data = localStorage.getItem(CACHE_KEY);
+  if (!data) return null;
+
+  try {
+    return JSON.parse(data);
+  } catch {
+    return null;
+  }
+};
+
+const setCache = (data) => {
+  localStorage.setItem(CACHE_KEY, JSON.stringify(data));
+  localStorage.setItem(CACHE_TIME_KEY, Date.now().toString());
+};
+
+const shouldFetch = () => {
+  const last = localStorage.getItem(CACHE_TIME_KEY);
+  if (!last) return true;
+  return Date.now() - Number(last) > MIN_FETCH_INTERVAL;
+};
+
+// ---------------- FETCH WALLETS ----------------
+export const fetchWallets = async () => {
+  const cached = getCache();
+
+  // Use cache if still valid
+  if (!shouldFetch() && cached) {
+    return cached;
+  }
+
+  // Internal function to fetch fresh data
+  const fetchNewData = async () => {
+    let headers = {};
+    
+    try {
+      const token = localStorage.getItem("NOBITEX_TOKEN");
+      if (!token) return [];
+      headers.Authorization = `Token ${token}`;
+      const url = `${WORKER_URL}/users/wallets/list`;
+      
+    const response = await axios.get(url, {
+      headers,
+      validateStatus: () => true,
+    });
+
+    let data = response.data?.wallets || [];
+
+    // Update cache and trigger callback if provided
+    if (data.length > 0) {
+      setCache(data);
+      //if (typeof onUpdate === "function") onUpdate(data);
+    }
+
+      return data;
+    } catch (err) {
+      console.error(`Fetch history failed :`, err);
+      return cached || [];
+    }
+  };
+
+  // If cache exists, return it immediately and refresh in background
+  if (cached) {
+    fetchNewData(); // background refresh
+    return cached;
+  }
+
+  // Otherwise, fetch and return immediately
+  return await fetchNewData();
+};
