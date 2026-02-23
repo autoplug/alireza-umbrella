3ca2e68c6eb8e5f0af69dd22e86eb62e45aae880
Alireza <alireza.b83@gmail.com>
2026-02-23 19:13:37 +0330

    Create useWallets.jsx

---
+++ src/hooks/useWallets.jsx
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchWallets } from "../api/fetchWallets";
+
+export const useTrades = () => {
+  return useQuery({
+    queryKey: ["wallets"],
+    queryFn: () => fetchWallets(),
+
+    staleTime: 5 * 60 * 1000,  // 5 minutes
+    gcTime: Infinity,           // keep cache forever
+    refetchOnMount: false,
+    refetchOnWindowFocus: true,
+    refetchOnReconnect: true,
+    placeholderData: (prev) => prev, // show previous data while fetching
+  });
+  
+  return {
+    wallets: data?.wallets || [],
+    lastUpdate: data?._lastUpdate || null,
+  };
+};
