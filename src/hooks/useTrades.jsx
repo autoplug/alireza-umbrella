a53561f52397709ca5d1fa0decde816083c03f6d
Alireza <alireza.b83@gmail.com>
2026-02-23 18:50:14 +0330

    Update useTrades.jsx

--- src/hooks/useTrades.jsx
+++ src/hooks/useTrades.jsx
@@ -1,9 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchTrades } from "../api/fetchTrades";
 
-export const useTrades = (symbol) => {
+export const useTrades = () => {
   return useQuery({
-    queryKey: ["trades", symbol],
+    queryKey: ["trades"],
     queryFn: () => fetchTrades(symbol),
 
     staleTime: 5 * 60 * 1000,  // 5 minutes
