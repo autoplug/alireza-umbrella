af65900271ec4e9d35ceb116ae65f8a62e2ea7e9
Alireza <alireza.b83@gmail.com>
2026-02-24 10:26:46 +0330

    Update useOrders.jsx

--- src/hooks/useOrders.jsx
+++ src/hooks/useOrders.jsx
@@ -1,14 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchOrders } from "../api/fetchOrders";
 
-export const useTrades = () => {
+export const useOrders = () => {
   const query = useQuery({
     queryKey: ["orders"],
     queryFn: () => fetchOrders(),
 
     staleTime: 5 * 60 * 1000,  // 5 minutes
     gcTime: Infinity,           // keep cache forever
-    refetchOnMount: false,
+    refetchOnMount: true,
     refetchOnWindowFocus: true,
     refetchOnReconnect: true,
     placeholderData: (prev) => prev, // show previous data while fetching
