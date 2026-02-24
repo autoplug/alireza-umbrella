f69369851b183aba1c10bc7c65113da75af85e61
Alireza <alireza.b83@gmail.com>
2026-02-24 19:49:58 +0330

    Update Chart.jsx

--- src/pages/Chart.jsx
+++ src/pages/Chart.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 //import CandleChart from "../components/CandleChart";
 //import TableOrder from "../components/TableOrder";
