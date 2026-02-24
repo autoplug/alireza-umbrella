9daa522ab1aa31c3119d322c118781b80ccce1a0
Alireza <alireza.b83@gmail.com>
2026-02-24 09:20:40 +0330

    Update fetchTrades.jsx

--- src/fetch/fetchTrades.jsx
+++ src/fetch/fetchTrades.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const WORKER_URL = "https://nobitex3.alireza-b83.workers.dev";
+const WORKER_URL = "https://nobitex.alireza-b83.workers.dev";
 const CACHE_TIME_KEY = "MARKETS_CACHE_TIME";
 // 5 minutes
 const MIN_FETCH_INTERVAL = 5 * 60 * 1000;
