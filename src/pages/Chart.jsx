20670b043e09869e3d0309bbba807229aaee11c9
Alireza <alireza.b83@gmail.com>
2026-02-24 16:49:33 +0330

    Update Chart.jsx

--- src/pages/Chart.jsx
+++ src/pages/Chart.jsx
@@ -1,14 +1,16 @@
 
 import CandleChart from "../components/CandleChart";
 import { useTrades } from "../hooks/useTrades";
+import { useOrders } from "../hooks/useOrders";
 
 export default function Chart() {
-
+    const { orders } = useOrders();
+    const { trades } = useTrades();
   return (
     <div style={{ padding: 16 }}>
         <h2>Chart</h2>
         {/* Additional  settings can be added here */}
-        <CandleChart symbol={"BTCRLS"} />
+        <CandleChart symbol={"BTCRLS"} orders={orders} trades={trades}/>
         
     </div>
   );
