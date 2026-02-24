c3240982fa45fbef0bc093b5cd77b93e7863dbab
Alireza <alireza.b83@gmail.com>
2026-02-24 10:41:52 +0330

    Update Header.jsx

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 
