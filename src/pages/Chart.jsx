f3b67321c3e8a48b141b7c560be52e55db1e3599
Alireza <alireza.b83@gmail.com>
2026-02-24 19:31:25 +0330

    Update Chart.jsx

--- src/pages/Chart.jsx
+++ src/pages/Chart.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useMemo } from "react";
 
 //import CandleChart from "../components/CandleChart";
-import TableOrder from "../components/TableOrder";
+//import TableOrder from "../components/TableOrder";
 import { useTrades } from "../hooks/useTrades";
-import { useOrders } from "../hooks/useOrders";
+//import { useOrders } from "../hooks/useOrders";
 
 export default function Chart() {
   const { trades } = useTrades();   // همه معاملات
-  const { orders } = useOrders();   // همه سفارشات فعال
+  //const { orders } = useOrders();   // همه سفارشات فعال
 
   const symbols = Array.from(
     new Set([
@@ -17,19 +17,7 @@ export default function Chart() {
 
   const [selectedSymbol, setSelectedSymbol] = useState(symbols[0]);
 
-  // فیلتر سفارشات بر اساس سمبل انتخابی
-  const filteredOrders = useMemo(() => {
-    return orders.filter(
-      (o) => o.market.toLowerCase() === selectedSymbol.toLowerCase()
-    );
-  }, [orders, selectedSymbol]);
 
-  // فیلتر آخرین 10 معامله
-  const filteredTrades = useMemo(() => {
-    return trades
-      .filter((t) => t.market.toLowerCase() === selectedSymbol.toLowerCase())
-      .slice(-10);
-  }, [trades, selectedSymbol]);
 
   return (
     <div>
@@ -57,12 +45,7 @@ export default function Chart() {
     
 
       {/* Orders Table */}
-      <h4>Active Orders</h4>
-      <TableOrder orders={filteredOrders} />
-
-      {/* Last 10 Trades */}
-      <h4>Last 10 Trades</h4>
-      <TableOrder orders={filteredTrades}/>
+    
     </div>
   );
 }
