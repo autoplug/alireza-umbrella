9f4a66630aa4fc5d4ae071720ea201e3c97687c7
Alireza <alireza.b83@gmail.com>
2026-02-25 10:24:37 +0330

    Update Chart.jsx

--- src/pages/Chart.jsx
+++ src/pages/Chart.jsx
@@ -24,7 +24,7 @@ export default function Chart() {
       if (!trades) return;
       
       const result = trades?.filter((trade) => {
-        const oneWeekAgo = Math.floor(Date.now()) - 7 * 24 * 60 * 60 * 1000;
+        const oneWeekAgo = Math.floor(Date.now()/ 1000) - 7 * 24 * 60 * 60;
         return (
           trade.market?.toLowerCase() === selectedSymbol?.toLowerCase() &&
           trade.timestamp >= oneWeekAgo
