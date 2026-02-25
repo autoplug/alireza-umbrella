e885b47965e70e9023a0d6e1ac97a8d07e572dac
Alireza <alireza.b83@gmail.com>
2026-02-25 17:17:08 +0330

    Update CandleChart.jsx

--- src/components/CandleChart.jsx
+++ src/components/CandleChart.jsx
@@ -170,6 +170,7 @@ export default function CandleChart({ symbol, orders, trades }) {
         <div
           style={{
             display: "flex",
+            width: "30%",
             borderRadius: "8px",
             overflow: "hidden",
             border: "1px solid #ccc",
