10e1f12f76f86accca69421b339bcb5d0c4b815b
Alireza <alireza.b83@gmail.com>
2026-02-27 17:45:50 +0330

    Create index.jsx

---
+++ src/pages/Trades/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+//import ProfitSummary from "../components/ProfitSummary"; // if you prefer a Markets component
+import ProcessSellPanel from "../components/ProcessSellPanel"; 
+
+export default function MarketPage() {
+  return (
+    <div>
+      {/* ProfitSummary section 
+      <ProfitSummary />  */}
+      
+      <ProcessSellPanel />
+  
+      <div style={{ height: "60px" }}></div>
+    </div>
+  );
+}
