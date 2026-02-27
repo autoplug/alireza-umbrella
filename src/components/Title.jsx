3ad01ab769bf3a4cf95f9d717f453a07f56b8b07
Alireza <alireza.b83@gmail.com>
2026-02-27 16:11:53 +0330

    Create Title.jsx

---
+++ src/components/Title.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function TitleBar({ title, count }) {
+  const barStyle = {
+    backgroundColor: "#EFF0F4",       // کرم روشن
+    borderTop: "2px solid #E7E7E7",   // کرم تیره‌تر
+    padding: "5px 10px",
+    margin: "0 0",
+    marginBottom : "10px",
+    display: "flex",
+    alignItems: "center",
+    gap: "6px",
+  };
+
+  const h3Style = {
+    margin: 0,
+    fontWeight: "bold",
+  };
+
+  return (
+    <div style={barStyle}>
+      <h3 style={h3Style}>
+        {title} {count > 0 ? `(${count})` : ""}
+      </h3>
+    </div>
+  );
+}
