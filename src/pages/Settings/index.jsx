a73a7385b9ece0711b78a06dc10ee8bfe36c577b
Alireza <alireza.b83@gmail.com>
2026-02-27 17:42:15 +0330

    Update index.jsx

--- src/pages/Settings/index.jsx
+++ src/pages/Settings/index.jsx
@@ -1,6 +1,16 @@
 import React, { useState } from "react";
 import NobitexToken from "./NobitexToken";
 
+const rowStyle = {
+  padding: "10px 0",
+  margin: "0 0",
+  marginTop: "20px",
+  overflowY: "auto" ,
+  backgroundColor: "#fff",
+  borderTop: "1px solid #CCC",
+  borderBottom: "1px solid #CCC",
+};
+
 export default function Settings() {
   const [token, setToken] = useState(localStorage.getItem("NOBITEX_TOKEN") || "");
 
@@ -10,7 +20,7 @@ export default function Settings() {
   };
 
   return (
-    <div style={{ padding: 16 }}>
+    <div style={{...rowStyle }}>
       <h2>Settings</h2>
       <NobitexToken initialToken={token} onSave={handleTokenSave} />
       {/* Additional settings can be added here */}
