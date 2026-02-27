58213d27f79bea840fe2c6bd954bd7cd71e8efd3
Alireza <alireza.b83@gmail.com>
2026-02-27 16:40:33 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ export default function App() {
       
       
       <Router>
-        <div style={{ minHeight: "100vh", paddingBottom: "60px", backgroundColor: "#FAF9F7" }}>
+        <div style={{ minHeight: "100vh", paddingBottom: "60px" }}>
           {/* Spacer for content */}
           <div style={{ height: "43px" }}></div>
           {/* Routes */}
