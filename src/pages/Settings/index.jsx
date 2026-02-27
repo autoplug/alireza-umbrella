2fc2a61a8e864d7dd9eaa2c35fefc39f536b27b7
Alireza <alireza.b83@gmail.com>
2026-02-27 14:53:59 +0330

    Update index.jsx

--- src/pages/Settings/index.jsx
+++ src/pages/Settings/index.jsx
@@ -1,1 +1,21 @@
-cfdc
+import React, { useState } from "react";
+import NobitexToken from "../../components/NobitexToken";
+
+function Settings() {
+  const [token, setToken] = useState(localStorage.getItem("NOBITEX_TOKEN") || "");
+
+  const handleTokenSave = (newToken) => {
+    localStorage.setItem("NOBITEX_TOKEN", newToken);
+    setToken(newToken);
+  };
+
+  return (
+    <div style={{ padding: 16 }}>
+      <h2>Settings</h2>
+      <NobitexToken initialToken={token} onSave={handleTokenSave} />
+      {/* Additional settings can be added here */}
+    </div>
+  );
+}
+
+export default Settings;
